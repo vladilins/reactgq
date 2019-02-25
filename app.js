b5a8641684461eeba5d7764c9b2ccca8023abe2f
Your Name <ilin96inbox@gmail.com>
2019-02-25 19:25:38 +0300

    created mongo Schema

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ mongoose
   .connect(
     `mongodb+srv://vladilin:${
       process.env.MONGO_PASSWORD
-    }@cluster0-jnjn2.mongodb.net/test?retryWrites=true`
+    }@cluster0-jnjn2.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`
   )
   .then(() => {
     app.listen(3000);
