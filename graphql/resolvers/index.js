9dc2f325ec2b1a5ff69fc4937f53b22d67ca6901
Your Name <ilin96inbox@gmail.com>
2019-02-27 17:47:55 +0300

    created booking query and events

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -2,6 +2,7 @@ const bcrypt = require("bcryptjs");
 
 const Event = require("../../models/event");
 const User = require("../../models/user");
+const Booking = require("../../models/booking");
 
 const events = async eventIds => {
   try {
@@ -43,6 +44,21 @@ module.exports = {
           _id: event.id,
           date: new Date(event._doc.date).toISOString(),
           creator: user.bind(this, event._doc.creator)
+        };
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+  bookings: async () => {
+    try {
+      const bookings = await Booking.find();
+      return bookings.map(booking => {
+        return {
+          ...booking._doc,
+          _id: booking.id,
+          createdAt: new Date(booking._doc.createdAt).toISOString,
+          updatedAt: new Date(booking._doc.updatedAt).toISOString
         };
       });
     } catch (err) {
@@ -99,5 +115,19 @@ module.exports = {
     } catch (err) {
       throw err;
     }
+  },
+  bookEvent: async args => {
+    const fetchedEvent = await Event.findOne({ _id: args.eventId });
+    const booking = new Booking({
+      user: "5c0fbd06c816781c518e4f3e",
+      event: fetchedEvent
+    });
+    const result = await booking.save();
+    return {
+      ...result._doc,
+      _id: result.id,
+      createdAt: new Date(booking._doc.createdAt).toISOString,
+      updatedAt: new Date(booking._doc.updatedAt).toISOString
+    };
   }
 };
