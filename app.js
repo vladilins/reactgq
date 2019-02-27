3a7be3d1307d43622835b04afbb48c29aa0fd5c1
Your Name <ilin96inbox@gmail.com>
2019-02-27 17:33:50 +0300

    created booking Schema

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql");
-
 const mongoose = require("mongoose");
 
 const graphQlSchema = require("./graphql/schema");
