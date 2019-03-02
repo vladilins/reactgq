d3e08862f196796eb710ffb30f1022cc41b85c09
Your Name <ilin96inbox@gmail.com>
2019-03-02 13:48:02 +0300

    create is-auth middleware and added it to express app

---
+++ middleware/is-auth.js
@@ -0,0 +1,30 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = (req, res, next) => {
+  const authHeader = req.get("Authorication");
+  if (!authHeader) {
+    req.isAuth = false;
+    return next();
+  }
+
+  const token = authHeader.split(" ")[1];
+  if (!token || token === "") {
+    req.isAuth = false;
+    return next();
+  }
+  let decodedToken;
+  try {
+    decodedToken = jwt.verify(token, "asdgagegrewgewr");
+  } catch {
+    req.isAuth = false;
+    return next();
+  }
+
+  if (!decodedToken) {
+    req.isAuth = false;
+    return next();
+  }
+  req.isAuth = true;
+  req.userId = decodedToken.userId;
+  next();
+};
