7e1e9f17e4c37f992aa9ac51571eabf6df4818b9
Your Name <ilin96inbox@gmail.com>
2019-03-04 18:12:03 +0300

    created reagistration form and style for it

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
 
+import "./Auth.css";
+
 class Auth extends Component {
   render() {
-    return <div>Auth</div>;
+    return (
+      <form className="auth-form" onSubmit={this.submitHandler}>
+        <div className="form-control">
+          <label htmlFor="email">E-Mail</label>
+          <input type="email" id="email" ref={this.emailEl} />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" ref={this.passwordEl} />
+        </div>
+        <div className="form-actions">
+          <button type="submit">Submit</button>
+          <button type="button" onClick={this.switchModeHandler}>
+            Switch to {this.state.isLogin ? "Signup" : "Login"}
+          </button>
+        </div>
+      </form>
+    );
   }
 }
 
